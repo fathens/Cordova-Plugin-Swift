7660c77731f6198fb36782109feba7352af766a7
Kunio Sawatani <devel@fathens.org>
2015-10-20 21:00:06 +0900

    Xcodeproj の save に () を付けてみる

--- platforms/ios/hooks/after_plugin_install-fix_xcodeproj.sh
+++ platforms/ios/hooks/after_plugin_install-fix_xcodeproj.sh
@@ -35,7 +35,7 @@ build_settings(project,
     "SWIFT_OBJC_BRIDGING_HEADER" => "${project_name}/Plugins/${plugin_id}/union-Bridging-Header.h"
 )
 
-project.save
+project.save()
 puts "Saved: #{project}"
 EOF
 
