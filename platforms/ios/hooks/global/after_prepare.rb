ee96aa49e88df611b4ebc99b49ea314331702127
Kunio Sawatani <devel@fathens.org>
2015-10-22 19:36:30 +0900

    after_prepare に Podfile への追加も実装

--- platforms/ios/hooks/global/after_prepare.rb
+++ platforms/ios/hooks/global/after_prepare.rb
@@ -3,56 +3,118 @@
 require 'rexml/document'
 require 'xcodeproj'
 
-platformDir = Pathname('platforms').join('ios')
+$PROJECT_DIR = Pathname('.').realpath
+$PLATFORM_DIR = Pathname('platforms').join('ios').realpath
 
-plugin_id = Pathname(ENV['CORDOVA_HOOK']).dirname.dirname.dirname.dirname.basename
+def plugin_id
+  curPlugin = Pathname(ENV['CORDOVA_HOOK']).dirname.dirname.dirname.dirname.dirname.join('plugin.xml')
+  curXml = REXML::Document.new(File.open(curPlugin))
+  return curXml.elements['plugin'].attributes['id']
+end
 
-union_file = Pathname.glob(platformDir.join('*').join('Plugins').join(plugin_id).join('union-Bridging-Header.h'))[0]
-puts "Union Header: #{union_file}:"
-
-lines = []
-File.open(union_file) { |f|
-  lines.concat f.readlines
-}
-Pathname.glob(Pathname('plugins').join('*').join('plugin.xml')).each { |xmlFile|
-  begin
-    xml = REXML::Document.new(File.open(xmlFile))
-    xml.elements.each('plugin/platform/bridging-header-file') { |elm|
-      src_path = xmlFile.dirname.join(elm.attributes['src'])
-      puts "Appending #{src_path}"
-      File.open(src_path) { |f|
+class AllPlugins
+  @header_files
+  @pods
+  @union_file
+  def initialize
+    @header_files = []
+    @pods = []
+    @union_file = Pathname.glob($PLATFORM_DIR.join('*').join('Plugins').join(plugin_id).join('union-Bridging-Header.h'))[0]
+    Pathname.glob($PROJECT_DIR.join('plugins').join('*').join('plugin.xml')).each { |xmlFile|
+      begin
+        xml = REXML::Document.new(File.open(xmlFile))
+        xml.elements.each('plugin/platform/bridging-header-file') { |elm|
+          @header_files << xmlFile.dirname.join(elm.attributes['src'])
+        }
+        xml.elements.each('plugin/platform/podfile/pod') { |elm|
+          @pods << elm
+        }
+      rescue => ex
+        puts "Error on '#{xmlFile}': #{ex.message}"
+      end
+    }
+  end
+  
+  def union_file
+    return @union_file
+  end
+  
+  def append_union_header
+    lines = []
+    @header_files.each { |file|
+      puts "Header #{file}"
+      File.open(file) { |f|
         lines.concat f.readlines
       }
     }
-  rescue => ex
-    puts "Error on '#{xmlFile}': #{ex.message}"
-  end
-}
-File.open(union_file, "w") { |dst|
-  dst << lines.uniq.join
-}
 
-def build_settings(project, params)
-  project.targets.each do |target|
-    target.build_configurations.each do |conf|
-      params.each do |key, value|
-        conf.build_settings[key] = value
-      end
-    end
+    puts "Union Header: #{union_file}"
+    File.open(union_file, "w+") { |dst|
+      lines.concat dst.readlines
+      dst << lines.uniq.join
+    }
+  end
+  
+  def append_podfile
+    lines = []
+    @pods.each { |elm|
+      args = [elm.attributes['name'], elm.attributes['version']]
+      puts "Pod #{args}"
+      line = args.select { |a|
+        a != nil
+      }.map { |a|
+        "'" + a + "'"
+      }.join(', ')
+      lines << "pod #{line}"
+    }
+    podfile = $PLATFORM_DIR.join('Podfile')
+    puts "Podfile: #{podfile}"
+    File.open(podfile, "a") { |dst|
+      dst << lines.uniq.join("\n")
+    }
   end
 end
 
-proj = Pathname.glob(platformDir.join('*.xcodeproj').to_path)[0]
-puts "Editing #{proj}"
+class FixXcodeproj
+  @project
+  def initialize(file)
+    puts "Editing #{file}"
+    
+    @project = Xcodeproj::Project.open(file)
+    @project.recreate_user_schemes
+  end
+  
+  def build_settings(params)
+    @project.targets.each do |target|
+      target.build_configurations.each do |conf|
+        params.each do |key, value|
+          conf.build_settings[key] = value
+        end
+      end
+    end
+  end
+  
+  def save
+    @project.save
+  end
+end
 
-project = Xcodeproj::Project.open(proj)
-project.recreate_user_schemes
-
-build_settings(project,
-"LD_RUNPATH_SEARCH_PATHS" => "\$(inherited) @executable_path/Frameworks",
-"OTHER_LDFLAGS" => "\$(inherited)",
-"ENABLE_BITCODE" => "NO",
-"SWIFT_OBJC_BRIDGING_HEADER" => union_file.relative_path_from(platformDir)
-)
-
-project.save
+if __FILE__ == $0
+  plugins = AllPlugins.new
+  plugins.append_union_header
+  plugins.append_podfile
+  
+  # On Platform Dir
+  Dir.chdir $PLATFORM_DIR
+  
+  system "pod install"
+  
+  xcode = FixXcodeproj.new(Pathname.glob('*.xcodeproj')[0])
+  xcode.build_settings(
+  "LD_RUNPATH_SEARCH_PATHS" => "\$(inherited) @executable_path/Frameworks",
+  "OTHER_LDFLAGS" => "\$(inherited)",
+  "ENABLE_BITCODE" => "NO",
+  "SWIFT_OBJC_BRIDGING_HEADER" => plugins.union_file.relative_path_from($PLATFORM_DIR)
+  )
+  xcode.save
+end
