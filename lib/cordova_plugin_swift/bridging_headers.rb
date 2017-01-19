71e77f5e63ab28edd09377400f298cf90c19fef5
Kunio SAWATANI <fsawa@fathens.org>
2017-01-19 02:16:22 +0000

    SWIFT_VERSION と Bridging-Header のみが対象
    Podfile は Cordova がオフィシャルに面倒見てくれるので、ここでの作業がかなり省かれました。

---
+++ lib/cordova_plugin_swift/bridging_headers.rb
@@ -0,0 +1,18 @@
+require 'rexml/document'
+
+module CordovaPluginSwift
+    def self.bridging_headers(base_dir)
+        Pathname.glob(base_dir/'plugins'/'*'/'plugin.xml').map { |xmlFile|
+            begin
+                xml = REXML::Document.new(File.open(xmlFile))
+                xml.get_elements('//platform[@name="ios"]/framework[@type="podspec"]').map { |e|
+                    e.attributes['import']
+                }
+            rescue => ex
+                puts "Error on '#{xmlFile}': #{ex.message}"
+            end
+        }.flatten.compact.uniq.map { |name|
+            "import <#{name}>"
+        }
+    end
+end
--- platforms/ios/hooks/after_prepare.rb
+++ platforms/ios/hooks/after_prepare.rb
@@ -4,78 +4,22 @@ require 'pathname'
 require 'rexml/document'
 require_relative '../../../lib/cordova_plugin_swift'
 
-def load_podfile(base_dir, ios_version, swift_version)
-    podfile = Pathname.glob(base_dir/'plugins'/'*'/'plugin.xml').map { |xmlFile|
-        begin
-            Podfile.from_pluginxml(xmlFile)
-        rescue => ex
-            puts "Error on '#{xmlFile}': #{ex.message}"
-        end
-    }.compact.reduce(Podfile.new(ios_version: ios_version, swift_version: swift_version)) { |a, b|
-        a.merge b
-    }
-
-    raise "Require down version of iOS in plugins: #{podfile.ios_version} < #{ios_version}" if podfile.ios_version < ios_version
-
-    podfile
-end
-
-def bridging(podfile, target_file)
-    headers = podfile.pods.map {|p| p.bridging_headers }.flatten
-
-    File.open(target_file, 'a') { |dst|
-        dst.puts()
-        dst.puts "// Below add by Cordova-Plugin-Swift"
-        dst.puts headers.map(&:to_s)
-    }
-end
-
-def removeImport(files)
-    files.each { |fileSrc|
-        fileDst = "#{fileSrc}.rm"
-            open(fileSrc, 'r') { |src|
-                open(fileDst, 'w') { |dst|
-                    src.each_line { |line|
-                        if line =~ /^import +Cordova$/ then
-                            puts "Removing '#{line.strip}' from #{fileSrc}"
-                        else
-                            dst.puts line
-                        end
-                    }
-                }
-            }
-        File.rename(fileDst, fileSrc)
-    }
-end
-
 $PROJECT_DIR = Pathname.pwd.realpath
 $PLATFORM_DIR = $PROJECT_DIR/'platforms'/'ios'
 
-config = ConfigXml.new($PROJECT_DIR/'config.xml')
-$APPLICATION_NAME = config.application_name
+swift_version = '3.0'
 
-log_header "Loading podfile..."
-podfile = load_podfile($PROJECT_DIR, config.ios_version || '9.0', '3.0')
-log "Using ios_version: #{podfile.ios_version}"
-log "Using swift_version: #{podfile.swift_version}"
-
-podfile.write($PLATFORM_DIR/'Podfile', $APPLICATION_NAME)
-Dir.chdir($PLATFORM_DIR) {
-    system "pod install"
-}
-
-log_header "Removing unnecessary imports..."
-removeImport Pathname.glob($PLATFORM_DIR/$APPLICATION_NAME/'Plugins'/'**'/'*.swift')
-
-bridging podfile, $PLATFORM_DIR/$APPLICATION_NAME/'Bridging-Header.h'
-
-log_header "Modify xcconfig..."
-
-open($PLATFORM_DIR/'cordova'/'build-extras.xcconfig', 'a') { |f|
+log "Using swift_version: #{swift_version}"
+open($PLATFORM_DIR/'cordova'/'build.xcconfig', 'a') { |f|
     f.puts "SWIFT_VERSION = #{podfile.swift_version}"
 }
-["debug", "release"].each { |key|
-    open($PLATFORM_DIR/'cordova'/"build-#{key}.xcconfig", 'a') { |f|
-        f.puts "\#include \"#{$PLATFORM_DIR/'Pods'/'Target Support Files'/"Pods-#{$APPLICATION_NAME}"/"Pods-#{$APPLICATION_NAME}.#{key}.xcconfig"}\""
+
+headers = CordovaPluginSwift.bridging_headers(base_dir)
+bridging_file = Pathname.glob($PLATFORM_DIR/'*'/'Bridging-Header.h').first
+if (bridging_file && !headers.empty?) then
+    File.open(bridging_file, 'a') { |dst|
+        dst.puts()
+        dst.puts "// Below add by cordova-plugin-swift"
+        dst.puts headers
     }
-}
+end
